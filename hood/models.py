cf73c0f319ff4ef7768e0d5fb654e27219576663
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-17 22:48:56 +0300

    ft:install pillow,countries and tinymce

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import datetime as dt
-
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from PIL import Image
+from django_countries.fields import CountryField
 
 # Create your models here.
