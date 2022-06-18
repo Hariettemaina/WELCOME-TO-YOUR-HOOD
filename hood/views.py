986b13593bc8f5cd9341af5612f885df1803688c
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-18 23:06:09 +0300

    ft: view function to define the index page

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+
 
 # Create your views here.
+
+def index(request):
+    date = dt.date.today()
+    all_neighborhoods = Neighborhood.get_neighborhoods()
+    
+    
+    
+    if 'neighborhood' in request.GET and request.GET["neighborhood"]:
+        neighborhoods = request.GET.get("neighborhood")
+        searched_neighborhood = Business.get_by_neighborhood(neighborhoods)
+        all_posts = Posts.get_by_neighborhood(neighborhoods)
+        message = f"{neighborhoods}"
+        all_neighborhoods = Neighborhood.get_neighborhoods()  
+        
+        return render(request, 'index.html', {"message":message,"location": searched_neighborhood,
+                                               "all_neighborhoods":all_neighborhoods, "all_posts":all_posts})
+    else:
+        message = "No Neighborhood Found!"  
+    
+    return render(request, 'index.html', {"date": date, "all_neighborhoods":all_neighborhoods,})
+ 
+        
