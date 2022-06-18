c1cbc061e4aceaba54e23ac2b8fd77fcb535c601
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-19 00:18:24 +0300

    ft:profile update and new neibhood forrms

--- hood/forms.py
+++ hood/forms.py
@@ -13,3 +13,18 @@ class NewBusinessForm(forms.ModelForm):
         widgets = {
           'address': forms.Textarea(attrs={'rows':1, 'cols':10,}),
         }
+        
+        
+class NewNeighborhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ['Admin', 'pub_date', 'admin_profile']
+            
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        widgets = {
+          'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
