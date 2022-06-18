0899bf2f423b1d4c6e62ff3c64b555205a308e63
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-19 00:28:07 +0300

    import forms and define the login page

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.templatetags.static import static
-from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import *
+from .forms import *
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 
@@ -32,4 +33,17 @@ def index(request):
     
     return render(request, 'index.html', {"date": date, "all_neighborhoods":all_neighborhoods,})
  
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('/')
         
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/registration_form.html', {'form':form})
+
+ 
