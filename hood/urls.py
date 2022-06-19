a8af02a0c1c0337fba747464732b61b13aee033d
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-19 12:57:10 +0300

    debugging

--- hood/urls.py
+++ hood/urls.py
@@ -1,9 +1,19 @@
-from django.conf.urls import path, include
+from django.urls import path, include
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
 urlpatterns = [
-    path('',views.index, name = 'index')
+    path('',views.index, name = 'index'),
+    path('search/', views.search_businesses, name='search_results'),
+    path('business/', views.get_business, name='business_results'),
+    path('new/business/', views.new_business, name='new-business'),
+    path('new/post/', views.new_post, name='new-post'),
+    path('accounts/profile/', views.user_profiles, name='profile'),
+   
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hood',
+    'crispy_forms',
+    'tinymce',
+    
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,10 @@ WSGI_APPLICATION = 'neighborhood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hoods',
+        'USER': 'carolmaina',
+    'PASSWORD':'ethan2017',
     }
 }
 
