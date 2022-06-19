39fd3933e2f570d7ba2999220f8ced53be2ddb0a
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-19 17:30:51 +0300

    register models

--- hood/admin.py
+++ hood/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+# Register your models here
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(NeighbourHood)
 admin.site.register(Business)
-admin.site.register(Neighborhood)
-admin.site.register(Profile)
-admin.site.register(Posts)
+admin.site.register(Post)
+admin.site.register(Contact)
